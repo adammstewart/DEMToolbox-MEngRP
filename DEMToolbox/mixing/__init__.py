3452ea967447c75ab6bbee8848dd71a22c7ecf12
adammstewart <axs2155@student.bham.ac.uk>
2026-04-11 12:07:50 +0100

    finally added old_lacey_mixing_curve and old_lacey_mixing_curve_fit to mixing __init__.py

--- DEMToolbox/mixing/__init__.py
+++ DEMToolbox/mixing/__init__.py
@@ -1,3 +1,3 @@
 from .lacey_mixing_index import macro_scale_lacey_mixing
-from .lacey_mixing_curve_fit import lacey_mixing_curve, lacey_mixing_curve_fit
+from .lacey_mixing_curve_fit import lacey_mixing_curve, lacey_mixing_curve_fit, old_lacey_mixing_curve, old_lacey_mixing_curve_fit
 from .homogeneity_index import homogeneity_index
